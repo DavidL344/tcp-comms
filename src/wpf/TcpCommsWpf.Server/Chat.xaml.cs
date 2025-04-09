7fb70026497ebd62f0c9101ae30fbb8e57f01050
David Langr <git@langr.tech>
2025-04-09 14:40:18 +0200

    Implement responding to messages on the server

--- src/wpf/TcpCommsWpf.Server/Chat.xaml.cs
+++ src/wpf/TcpCommsWpf.Server/Chat.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Net.Sockets;
 using System.Text;
 using System.Windows;
+using System.Windows.Input;
 
 namespace TcpCommsWpf.Server;
 
@@ -9,15 +10,17 @@ public partial class Chat : Window
     public required TcpClient Client { get; init; }
     public required IProgress<string> Progress { get; init; }
     public required CancellationToken CancellationToken { get; init; }
+    private NetworkStream _stream;
+    private readonly IProgress<string> _messages;
     
     public Chat()
     {
         InitializeComponent();
-        IProgress<string> messages = new Progress<string>(message =>
+        _messages = new Progress<string>(message =>
         {
-            ListView.Items.Add(message);
+            MessageView.Items.Add(message);
         });
-        _ = Task.Run(() => HandleClientAsync(messages), CancellationToken);
+        _ = Task.Run(() => HandleClientAsync(_messages), CancellationToken);
     }
     
     private async Task HandleClientAsync(IProgress<string> messages)
@@ -25,24 +28,18 @@ public partial class Chat : Window
         using (Client)
         {
             var buffer = new byte[1024];
-            var stream = Client.GetStream();
+            _stream = Client.GetStream();
             
             try
             {
                 int bytesRead;
-                while ((bytesRead = await stream.ReadAsync(buffer, CancellationToken)) > 0)
+                while ((bytesRead = await _stream.ReadAsync(buffer, CancellationToken)) > 0)
                 {
                     var received = Encoding.UTF8.GetString(buffer, 0, bytesRead);
                     if (received.Trim() == string.Empty) continue;
                     
                     Progress.Report($"Message received: \"{received.Trim()}\"");
-                    messages.Report($"[Client]: {received}");
-                    
-                    var payload = $"Hello from the server! Received message: {received}\n";
-                    
-                    var response = Encoding.UTF8.GetBytes(payload);
-                    await stream.WriteAsync(response, CancellationToken);
-                    
+                    messages.Report($"[Client ({DateTime.Now.Hour}:{DateTime.Now.Minute})]: {received}");
                 }
                 Progress.Report("Connection closed.");
             }
@@ -51,5 +48,23 @@ public partial class Chat : Window
                 Progress.Report($"Error processing client: {e.Message}");
             }
         }
+    }
+
+    private void MessageSend(object sender, RoutedEventArgs e)
+    {
+        _messages.Report($"[Server ({DateTime.Now.Hour}:{DateTime.Now.Minute})]: {MessageBox.Text}");
+        var payload = $"{MessageBox.Text}\n";
+        var response = Encoding.UTF8.GetBytes(payload);
+        
+        _stream.WriteAsync(response, CancellationToken).ConfigureAwait(false);
+        Progress.Report($"Message sent: \"{MessageBox.Text}\"");
+        MessageBox.Text = string.Empty;
+    }
+
+    private void MessageBox_OnKeyDown(object sender, KeyEventArgs e)
+    {
+        if (e.Key != Key.Enter) return;
+        e.Handled = true;
+        MessageSend(sender, e);
     }
 }
