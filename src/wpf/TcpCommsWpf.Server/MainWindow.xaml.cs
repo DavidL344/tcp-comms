e703fa49b65c4e3ddd10e05fe463ecd99c2febb1
David Langr <git@langr.tech>
2025-04-10 12:49:51 +0200

    Fix the shared library's namespace

--- src/wpf/TcpCommsWpf.Server/MainWindow.xaml.cs
+++ src/wpf/TcpCommsWpf.Server/MainWindow.xaml.cs
@@ -1,7 +1,7 @@
 ﻿using System.Net;
 using System.Net.Sockets;
 using System.Windows;
-using TcpComms.Shared;
+using TcpCommsWpf.Shared;
 
 namespace TcpCommsWpf.Server;
 
--- src/wpf/TcpCommsWpf.Shared/Chat.xaml.cs
+++ src/wpf/TcpCommsWpf.Shared/Chat.xaml.cs
@@ -3,7 +3,7 @@ using System.Text;
 using System.Windows;
 using System.Windows.Input;
 
-namespace TcpComms.Shared;
+namespace TcpCommsWpf.Shared;
 
 public partial class Chat : Window
 {
